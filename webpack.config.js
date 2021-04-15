3426231a88762a16381fa531dbade875bd5cf67e
unknown <km_si@ymail.com>
2021-04-15 12:50:38 -0400

    convert the app to pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
@@ -50,6 +50,7 @@ const config = {
       name: "Food Event",
       short_name: "Foodies",
       description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
       background_color: "#01579b",
       theme_color: "#ffffff",
       fingerprints: false,
@@ -60,7 +61,7 @@ const config = {
         destination: path.join("assets", "icons")
       }]
     })
-  ],
+],
   mode: "development"
 };
 
